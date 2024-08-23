963a77ee7cb827414baf8ea1f4f7debaae895402
mahdicode866 <mis861400@gmail.com>
2024-08-23 23:13:06 +0330

    my chganges
    this is a new change on my web application

---
+++ mahdi/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+# def hello(request):
+#     return render(request,'index.html')
+
+
+
+# def hello(request):
+    # temp=loader.get_template('index.html')
+    # return HttpResponse(temp.render())
+    
+    
+# def hello(request):
+#     temp=loader.get_template('index.html')
+#     return HttpResponse(temp.render())
+
+
+def hello (request):
+    return render(request,'index.html')
+    
