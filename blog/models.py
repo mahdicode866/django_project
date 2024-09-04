0b6bb11dfad8538c4151d5aef6cd36af00f9c326
mahdicode866 <mis861400@gmail.com>
2024-09-04 14:45:26 +0330

    this is adding tables of data base
    add table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. -> ok brother i want it to 
+
+class post(models.Model):
+     # image
+    # author 
+    title=models.CharField(max_length=255)
+    content=models.TextField()
+    # tag
+    # category
+    counted_views=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)      
+    published_date=models.DateTimeField(null=True)
+
